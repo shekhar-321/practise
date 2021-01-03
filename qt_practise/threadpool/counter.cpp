2ae4e23ff7e4249d46af99696ae48f74b329f4b4
Shekhar Singh <ssi@vectors.com>
2021-01-04 02:10:52 +0530

    threadpool example , same threads being used again and again instead of creating more.

---
+++ qt_practise/threadpool/counter.cpp
@@ -0,0 +1,17 @@
+#include "counter.h"
+
+
+void counter::run()
+{
+    qInfo() << "Starting: " << QThread::currentThread();
+
+    for (int i = 0; i < 5; i++) {
+        //comment this out to see the threads being reused
+        qInfo() << QThread::currentThread() << " = " << i;
+        auto value = static_cast<unsigned long>(QRandomGenerator::global()->bounded(100));
+        QThread::currentThread()->msleep(value);
+
+    }
+
+    qInfo() << "Finished: " << QThread::currentThread();
+}
